e914cb6652125c0bc6290ba9495b1ab54da9fb3a
Tryncha <juanes.aguirre26@gmail.com>
2026-09-09 15:22:57 -0500

    add some extra validations to subscription form

--- src/sections/subscription.tsx
+++ src/sections/subscription.tsx
@@ -1,12 +1,27 @@
 'use client';
 
 import Image from 'next/image';
-import { useId } from 'react';
+import { useId, useState } from 'react';
 
 const Subscription = () => {
   const firstNameId = useId();
   const lastNameId = useId();
   const emailId = useId();
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [isError, setIsError] = useState(false);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+
+  function handleSubmit(event: React.SubmitEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    // handle custom validation...
+    // setIsError(false);
+
+    if (!isError) {
+      setShowSuccessMessage(true);
+    }
+  }
 
   return (
     <section
@@ -24,55 +39,67 @@ const Subscription = () => {
           height={500}
           className="hidden xl:block"
         />
-        <form className="mt-8 flex flex-col gap-4">
-          <div className="flex flex-col">
-            <label
-              htmlFor={firstNameId}
-              className="text-sm"
-            >
-              First Name
-            </label>
-            <input
-              id={firstNameId}
-              type="text"
-              className="h-8 w-xs rounded-md border border-cyan-700 bg-white px-2"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label
-              htmlFor={lastNameId}
-              className="text-sm"
-            >
-              Last Name
-            </label>
-            <input
-              id={lastNameId}
-              type="text"
-              className="h-8 w-xs rounded-md border border-cyan-700 bg-white px-2"
-            />
-          </div>
-          <div className="flex flex-col">
-            <label
-              htmlFor={emailId}
-              className="text-sm"
-            >
-              Email
-            </label>
-            <input
-              id={emailId}
-              type="email"
-              className="h-8 w-xs rounded-md border border-cyan-700 bg-white px-2"
-            />
-          </div>
 
-          <button
-            type="submit"
-            onClick={(e) => e.preventDefault()}
-            className="rounded-full bg-cyan-700 px-4 py-2 text-lg font-medium text-white hover:cursor-pointer"
+        <div className="flex flex-col">
+          <form
+            onSubmit={handleSubmit}
+            className="mt-8 flex flex-col gap-4"
           >
-            Subscribe!
-          </button>
-        </form>
+            <div className="flex flex-col">
+              <label
+                htmlFor={firstNameId}
+                className="text-sm"
+              >
+                First Name
+              </label>
+              <input
+                id={firstNameId}
+                type="text"
+                className="h-8 w-xs rounded-md border border-cyan-700 bg-white px-2"
+                required
+              />
+            </div>
+            <div className="flex flex-col">
+              <label
+                htmlFor={lastNameId}
+                className="text-sm"
+              >
+                Last Name
+              </label>
+              <input
+                id={lastNameId}
+                type="text"
+                className="h-8 w-xs rounded-md border border-cyan-700 bg-white px-2"
+              />
+            </div>
+            <div className="flex flex-col">
+              <label
+                htmlFor={emailId}
+                className="text-sm"
+              >
+                Email
+              </label>
+              <input
+                id={emailId}
+                type="email"
+                className="h-8 w-xs rounded-md border border-cyan-700 bg-white px-2"
+                required
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="rounded-full bg-cyan-700 px-4 py-2 text-lg font-medium text-white hover:cursor-pointer"
+            >
+              Subscribe!
+            </button>
+          </form>
+
+          {/* Subscription Notification */}
+          {showSuccessMessage && (
+            <span className="mt-2 w-full text-center font-bold text-cyan-800">Subscription successfull!</span>
+          )}
+        </div>
       </div>
     </section>
   );
