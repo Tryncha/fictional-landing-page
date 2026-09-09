d5e5797695849b57bffdcca7a4d7ab1a2a3eda50
Tryncha <juanes.aguirre26@gmail.com>
2026-09-09 15:08:41 -0500

    add try/catch prevention to api request

--- src/lib/open-library.ts
+++ src/lib/open-library.ts
@@ -1,21 +1,26 @@
 import { BookResponse } from '@/types';
 
 export async function getBooks({ subject, limit }: { subject: string; limit: number }) {
-  const res = await fetch(`https://openlibrary.org/search.json?subject=${subject}&limit=${limit}`);
+  try {
+    const res = await fetch(`https://openlibrary.org/search.json?subject=${subject}&limit=${limit}`);
 
-  if (!res.ok) {
-    throw new Error('Failed to fetch books...');
+    if (!res.ok) {
+      throw new Error('Failed to fetch books...');
+    }
+
+    const data = await res.json();
+    const booksData: BookResponse[] = data.docs;
+
+    return booksData.map((book) => ({
+      key: book.key,
+      title: book.title,
+      language: book.language,
+      edition_count: book.edition_count,
+      first_publish_year: book.first_publish_year,
+      coverUrl: `https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`
+    }));
+  } catch (error) {
+    console.error(error);
+    return [];
   }
-
-  const data = await res.json();
-  const booksData: BookResponse[] = data.docs;
-
-  return booksData.map((book) => ({
-    key: book.key,
-    title: book.title,
-    language: book.language,
-    edition_count: book.edition_count,
-    first_publish_year: book.first_publish_year,
-    coverUrl: `https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`
-  }));
 }
